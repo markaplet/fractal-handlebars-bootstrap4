b0d5392edb01e043aae3270fa7de1edb209b1c5d
Mark Aplet <mark@visual28.com>
2021-07-01 14:58:52 -0700

    Reorganized bootstrap imports to more closely match original.

--- config/assets/bootstrap/bootstrap-sass.js
+++ config/assets/bootstrap/bootstrap-sass.js
@@ -7,46 +7,48 @@
 
 module.exports = [
     // Reset and dependencies
+    //--------------------------------------------------
     'node_modules/bootstrap/scss/_root.scss',
     'node_modules/bootstrap/scss/_reboot.scss',
     'node_modules/bootstrap/scss/_print.scss',
 
-    // Core CSS
-    'node_modules/bootstrap/scss/_type.scss',
-    'node_modules/bootstrap/scss/_images.scss',
-    'node_modules/bootstrap/scss/_grid.scss',
-    'node_modules/bootstrap/scss/_tables.scss',
-    'node_modules/bootstrap/scss/_forms.scss',
-    'node_modules/bootstrap/scss/_buttons.scss',
-    'node_modules/bootstrap/scss/_transitions.scss',
-
     // Components
-    // 'node_modules/bootstrap/scss/_code.scss',
-    'node_modules/bootstrap/scss/_dropdown.scss',
+    //--------------------------------------------------
+    'node_modules/bootstrap/scss/_alert.scss',
+    // 'node_modules/bootstrap/scss/_badge.scss',
+    'node_modules/bootstrap/scss/_breadcrumb.scss',
     'node_modules/bootstrap/scss/_button-group.scss',
-    'node_modules/bootstrap/scss/_input-group.scss',
+    'node_modules/bootstrap/scss/_buttons.scss',
+    // 'node_modules/bootstrap/scss/_card.scss',
+    // 'node_modules/bootstrap/scss/_code.scss',
+    'node_modules/bootstrap/scss/_close.scss',
     // 'node_modules/bootstrap/scss/_custom-forms.scss',
+    'node_modules/bootstrap/scss/_dropdown.scss',
+    'node_modules/bootstrap/scss/_forms.scss',
+    'node_modules/bootstrap/scss/_grid.scss',
+    'node_modules/bootstrap/scss/_images.scss',
+    'node_modules/bootstrap/scss/_input-group.scss',
+    // 'node_modules/bootstrap/scss/_jumbotron.scss',
+    // 'node_modules/bootstrap/scss/_list-group.scss',
+    // 'node_modules/bootstrap/scss/_media.scss',
     // 'node_modules/bootstrap/scss/_nav.scss',
     // 'node_modules/bootstrap/scss/_navbar.scss',
-    // 'node_modules/bootstrap/scss/_card.scss',
-    'node_modules/bootstrap/scss/_breadcrumb.scss',
     'node_modules/bootstrap/scss/_pagination.scss',
-    // 'node_modules/bootstrap/scss/_badge.scss',
-    //'node_modules/bootstrap/scss/_jumbotron.scss',
-    'node_modules/bootstrap/scss/_alert.scss',
-    //'node_modules/bootstrap/scss/_progress.scss',
-    // 'node_modules/bootstrap/scss/_media.scss',
-    //'node_modules/bootstrap/scss/_list-group.scss',
-    'node_modules/bootstrap/scss/_close.scss',
+    // 'node_modules/bootstrap/scss/_progress.scss',
+    'node_modules/bootstrap/scss/_tables.scss',
+    'node_modules/bootstrap/scss/_transitions.scss',
+    'node_modules/bootstrap/scss/_type.scss',
     // 'node_modules/bootstrap/scss/_toasts.scss',
-
+    
     // Components w/ JavaScript
+    //--------------------------------------------------
     // 'node_modules/bootstrap/scss/_modal.scss',
-    //'node_modules/bootstrap/scss/_tooltip.scss',
-    //'node_modules/bootstrap/scss/_popover.scss',
-    //'node_modules/bootstrap/scss/_carousel.scss',
-    //'node_modules/bootstrap/scss/_spinners.scss',
+    // 'node_modules/bootstrap/scss/_tooltip.scss',
+    // 'node_modules/bootstrap/scss/_popover.scss',
+    // 'node_modules/bootstrap/scss/_carousel.scss',
+    // 'node_modules/bootstrap/scss/_spinners.scss',
 
     // Utility classes
+    //--------------------------------------------------
     'node_modules/bootstrap/scss/utilities/*.scss',
 ];
